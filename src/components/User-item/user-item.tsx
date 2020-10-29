00834f27802d5e8d8a1b815407bf27f96974ecd9
Davi Santos Freire <davisfreire@academico.ufs.br>
2020-10-29 02:28:41 -0300

    Implementing features to the dashboard, changes to the user-item and added fontawesome for Icons

--- src/components/User-item/user-item.tsx
+++ src/components/User-item/user-item.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import { Card, Button } from 'react-bootstrap';
+import React, { useState, FormEvent } from 'react';
+import { Card, Button, Modal, Form, FormControl } from 'react-bootstrap';
 
 import './styles.scss';
+import api from '../../services/api';
 
 export interface User {
     email: string,
@@ -12,17 +13,60 @@ export interface User {
 }
 
 export default function UserItem(props: User) {
+    const [show, setShow] = useState(false);
+    const [name, setName] = useState('');
+    const [job, setJob] = useState('');
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    function handleUpdate(event: FormEvent) {
+        event.preventDefault();
+
+        api.post('users', {
+            name,
+            job
+        }).then(() => {
+            alert('User updated success');
+            handleClose();
+        }).catch(() => {
+            alert('Not valid');
+        })
+    }
+
     return (
         <Card className="card-user">
             <Card.Img variant="top" src={props.avatar} />
             <Card.Body>
-                <Card.Title>{props.first_name}{props.last_name}</Card.Title>
+                <Card.Title>{props.first_name} {props.last_name}</Card.Title>
                 <Card.Text>
                     {props.email}
                 </Card.Text>
-                <Button variant="primary" style={{ margin: '0 15px 0 0' }}>Update</Button>
-                <Button variant="secondary" style={{ padding: '.375rem .95rem' }}>Delete</Button>
+                <Button variant="primary" onClick={handleShow}>Update</Button>
             </Card.Body>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Update on {props.first_name} {props.last_name}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form onSubmit={handleUpdate}>
+                        <Form.Label htmlFor="formName">Name</Form.Label>
+                        <FormControl type="text" placeholder="Name" className="mr-sm-2" onChange={(event) => { setName(event.target.value) }} />
+                        <Form.Label htmlFor="formJob">Job</Form.Label>
+                        <FormControl type="text" placeholder="Job" className="mr-sm-2" onChange={(event) => { setJob(event.target.value) }} />
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={handleClose}>
+                                Close
+                            </Button>
+                            <Button variant="primary" type="submit" onSubmit={handleUpdate}>
+                                Save Changes
+                            </Button>
+                        </Modal.Footer>
+                    </Form>
+                </Modal.Body>
+            </Modal>
+
         </Card>
     );
-} 
+}
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,9 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, useEffect } from 'react';
 import api from '../../services/api';
 import { Navbar, Form, Button, FormControl } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash, faSearch } from '@fortawesome/free-solid-svg-icons';
 
 import logoImg from '../../images/logo-white.svg';
 import UserItem, { User } from '../../components/User-item/user-item';
@@ -11,30 +13,51 @@ import './styles.scss';
 export default function Dashboard() {
     const [users, setUsers] = useState([]);
 
-    async function searchUsers(event: FormEvent) {
-        event.preventDefault();
+    useEffect(() => {
+        api.get('users').then(response => {
+            setUsers(response.data.data);
+        })
+    }, []);
 
-        const response = await api.get('users');
-
-        setUsers(response.data.data)
+    function deleteUser(id: any) {
+        api.delete(`users/${id}`).then(() => {
+            alert('User deleted success');
+            users.splice((id - 1), 1);
+            setUsers(users);
+        })
     }
 
     return (
-        <div>
+        <>
             <div id="header">
                 <Navbar expand="lg" className="Navbar">
                     <Link to="/"><img src={logoImg} alt="Fasitecando" /></Link>
                     <Form inline>
                         <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                        <Button variant="light" onClick={searchUsers}>Search</Button>
+                        <Button variant="light">
+                            <FontAwesomeIcon icon={faSearch} />
+                        </Button>
                     </Form>
                 </Navbar>
             </div>
             <div id="grid-users">
                 {users.map((users: User) => (
-                    <UserItem key={users.id} email={users.email} first_name={users.first_name} last_name={users.last_name} id={users.id} avatar={users.avatar} />
-                ))}
+                    <div id="card-users">
+                        <Button variant="light" onClick={() => deleteUser(users.id)} className="delete-button">
+                            <FontAwesomeIcon icon={faTrash} />
+                        </Button>
+                        <UserItem key={users.id}
+                            email={users.email}
+                            first_name={users.first_name}
+                            last_name={users.last_name}
+                            id={users.id}
+                            avatar={users.avatar}
+                        />
+                    </div>
+                ))
+                }
             </div>
-        </div>
+        </>
     );
 }
+
